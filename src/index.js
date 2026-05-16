ae67e488a42c713726ec11475e12d91c8df59195
K C Ghoraav <144238131+ghoraav@users.noreply.github.com>
2026-05-16 17:58:57 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const app = require("./app");
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
--- src/utils/formatMessage.js
+++
@@ -1,7 +0,0 @@
-function formatMessage(req, resTime) {
-
-  return `${req.method} ${req.originalUrl} | Status: ${req.statusCode} | ${resTime}ms`;
-
-}
-
-module.exports = formatMessage;
